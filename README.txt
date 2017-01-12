ff7976f8181e87f33499576977b24bbba2f04246
kim-minjoo <kim.minjoo@nhn.com>
2017-01-12 18:08:36 +0900

    하둡 스터디 프로젝트 생성

---
+++ README.txt
@@ -0,0 +1,1 @@
+HADOOP STUDY
---
+++ src/main/java/com/naver/Main.java
@@ -0,0 +1,19 @@
+/*
+ *@(#)Main.java 2017.01.12
+ *
+ * Copyright 2017 NHN Corp. All rights Reserved.
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.naver;
+
+/**
+ *
+ *
+ * @author kim.minjoo
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
